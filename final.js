ede25cd5cbc9f007403b8522007eaaae828c5545
Lucia Azad <lucia.azad@gmail.com>
2015-12-13 22:12:26 -0500

    added final pages

---
+++ final.js
@@ -0,0 +1,8 @@
+var myAlert = alert("WARNING WARNING, you are about to visit a pretty mediocre website...");
+console.log(myAlert);
+
+document.write("<p> Please Enjoy </p>");
+
+function addWords(){
+  var other = alert("I have acknowledged your inuput, thank you.");
+}
